4c4380a68e4ebfca6ee3da3f1cebb88fa0d4ce2a
Kauã Santos <kavillykaua@gmail.com>
2025-11-07 11:03:33 -0300

    Delete e_commerce directory

--- e_commerce/caminho_arquivos/caminhos.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Paths:
-    base_dir = os.path.dirname(__file__)
-    save_clientes = os.path.join(base_dir, 'clientes.json')
-    save_adms = os.path.join(base_dir, 'adms.json')
-    save_produtos = os.path.join(base_dir, 'produtos.json')
-    save_carrinho = os.path.join(base_dir, 'carrinho.json')
