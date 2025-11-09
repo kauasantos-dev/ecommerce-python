6c93e9db7382a5f13ba8d99eef1c137e3cd52106
Kaua Santos <kavillykaua@gmail.com>
2025-11-09 13:36:18 -0300

    fix: adiciona imports necessários para funcionamento das funções

--- funcoes/pagamento/formas_pagamento.py
+++ funcoes/pagamento/formas_pagamento.py
@@ -1,7 +1,8 @@
+from ecommerce.usuarios import validadores
 def pix(produto):
     while True:
         try:
-            preco_digitado = Validador.validar_preco(input("Digite o preço do produto: "))
+            preco_digitado = validadores.ValidarProduto.validar_preco(input("Digite o preço do produto: "))
             break
         except ValueError as erro:
             print("Erro", erro, "Tente novamente.\n")
@@ -18,7 +19,7 @@ def cartao():
             nome = input("Nome no cartão (exatamente como consta no cartão): ")
             validade = input("Validade: ")
             cvv = input("Código de segurança: ")
-            ValidarEnderecoECartao.validar_cartao(numero, nome, validade, cvv)
+            validadores.ValidarEnderecoECartao.validar_cartao(numero, nome, validade, cvv)
             break
         except ValueError as erro:
             print("Erro: ", erro, " Tente novamente.\n")
